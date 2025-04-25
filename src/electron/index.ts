31eda85bc21c5d24c39e0f7c5c24d49e0a7a8c9d
Chris Witte <cwitte@xmlhq.com>
2025-04-25 14:08:34 -0500

    go.rr1.us

--- src/electron/index.ts
+++ src/electron/index.ts
@@ -20,7 +20,9 @@ app.on('ready', () => {
   // load a website to display
   //eWindow.loadURL(`https://www.google.com`);
   //eWindow.loadURL(`file://${__dirname}/../public/index.html`);
-  eWindow.loadURL('https://cf.derby.rr1.us');
+
+  //eWindow.loadURL('https://cf.derby.rr1.us');
+  eWindow.loadURL('https://go.rr1.us');
   //eWindow.loadURL('http://0.0.0.0:8080');
   //eWindow.webContents.openDevTools()
 
